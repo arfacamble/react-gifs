be2b02b488a0b22be18f83e966b9cbf6becef2b5
Arthur Campbell <arfa.camble@gmail.com>
2021-04-12 18:31:28 +0100

    more destructuring

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 
 class Gif extends Component {
   handleClick = (event) => {
-    this.props.selectGif(event.target.id);
+    const { selectGif } = this.props;
+    selectGif(event.target.id);
   }
 
   render() {
-    const src = `https://media3.giphy.com/media/${this.props.id}/giphy.gif`;
+    const { id } = this.props;
+    const src = `https://media3.giphy.com/media/${id}/giphy.gif`;
     return (
-      <img src={src} className="gif" id={this.props.id} alt="FUNNY GIF!!!" onClick={this.handleClick} />
+      <img src={src} className="gif" id={id} alt="FUNNY GIF!!!" onClick={this.handleClick} />
     );
   }
 }
